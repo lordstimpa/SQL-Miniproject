294fc6809f3952289c98a3881e9f3e0fb94d76d2
Steven Dalfall <steven.dalfall@live.se>
2023-03-04 14:05:14 +0100

    Updated database dump file

--- database.sql
+++ database.sql
@@ -1,17 +1,12 @@
 ALTER TABLE "public"."sda_project_person" DROP CONSTRAINT "FK_sda_project_person_project_id";
-ALTER TABLE "public"."sda" DROP CONSTRAINT "FK_sda_person_project_person_id";
+ALTER TABLE "public"."sda_project_person" DROP CONSTRAINT "FK_sda_person_project_person_id";
 DROP TABLE IF EXISTS "public"."sda_project";
-DROP TABLE IF EXISTS "public"."sda_person";
 DROP TABLE IF EXISTS "public"."sda_project_person";
+DROP TABLE IF EXISTS "public"."sda_person";
 CREATE TABLE "public"."sda_project" ( 
   "id" SERIAL,
   "project_name" VARCHAR(50) NOT NULL,
   CONSTRAINT "sda_project_pkey" PRIMARY KEY ("id")
-);
-CREATE TABLE "public"."sda_person" ( 
-  "id" SERIAL,
-  "person_name" VARCHAR(25) NOT NULL,
-  CONSTRAINT "sda_person_pkey" PRIMARY KEY ("id")
 );
 CREATE TABLE "public"."sda_project_person" ( 
   "id" SERIAL,
@@ -20,5 +15,18 @@ CREATE TABLE "public"."sda_project_person" (
   "hours" INTEGER NOT NULL,
   CONSTRAINT "sda_project_person_pkey" PRIMARY KEY ("id")
 );
+CREATE TABLE "public"."sda_person" ( 
+  "id" SERIAL,
+  "person_name" VARCHAR(25) NOT NULL,
+  CONSTRAINT "sda_person_pkey" PRIMARY KEY ("id")
+);
+INSERT INTO "public"."sda_project" ("project_name") VALUES ('Webshop');
+INSERT INTO "public"."sda_project" ("project_name") VALUES ('Bank Application');
+INSERT INTO "public"."sda_project_person" ("project_id", "person_id", "hours") VALUES (4, 2, 5);
+INSERT INTO "public"."sda_project_person" ("project_id", "person_id", "hours") VALUES (4, 2, 4);
+INSERT INTO "public"."sda_project_person" ("project_id", "person_id", "hours") VALUES (5, 2, 8);
+INSERT INTO "public"."sda_person" ("person_name") VALUES ('Steven');
+INSERT INTO "public"."sda_person" ("person_name") VALUES ('Erik');
+INSERT INTO "public"."sda_person" ("person_name") VALUES ('Anders');
 ALTER TABLE "public"."sda_project_person" ADD CONSTRAINT "FK_sda_project_person_project_id" FOREIGN KEY ("project_id") REFERENCES "public"."sda_project" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
 ALTER TABLE "public"."sda_project_person" ADD CONSTRAINT "FK_sda_person_project_person_id" FOREIGN KEY ("person_id") REFERENCES "public"."sda_person" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
